3bc11602801ea2a530adaddbefb812d4bfd5ecf7
Koyel Nandi <mr.admin@Koyels-Air.hitronhub.home>
2020-01-10 21:53:25 -0700

    initial commit for useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useReducer} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PostList from './post/PostList'
+import CreatePost from './post/CreatePost'
+import UserBar from './user/UserBar'
+
+
+
+const defaultPosts = [
+    { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Koyel Nandi' },
+    { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Koyel Nandi' }
+]
+
+function userReducer (state, action) {
+
+    switch (action.type) {
+        case 'LOGIN':
+        case 'REGISTER':
+            return action.username;
+        case 'LOGOUT':
+            return '';
+        default:
+            throw new Error()
+    }
 }
 
-export default App;
+
+export default function App () {  
+  
+  const [ user, dispatchUser ] = useReducer(userReducer, '');
+  const [ posts, setPosts ] = useState(defaultPosts);
+
+   return (        
+   <div style={{ padding: 8 }}>
+
+    <UserBar user={user} dispatch={dispatchUser} />            
+    <br />            
+    {user && <CreatePost user={user} posts={posts} setPosts={setPosts}/>}         
+    <br />            
+    <hr />
+
+    <PostList posts={posts} />
+        </div>
+    )
+}
+
